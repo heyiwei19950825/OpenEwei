50054b1a9a1ad58650f896154978b345089b2697
ewei <lamp_heyiwei@163.com>
2019-02-26 18:05:07 +0800

    扩展 文件 添加git

---
+++ extend/org/Helper.php
@@ -0,0 +1,215 @@
+<?php
+/**----------------------------------------------------------------------
+ * OpenEwei V1
+ * Copyright 2018-2018 http://www.redkylin.con All rights reserved.
+ * ----------------------------------------------------------------------
+ * Author: ewei(lamp_heyiwei@163.com)
+ * Date: 2018/12/18
+ * Time: 11:08
+ * ----------------------------------------------------------------------
+ */
+
+namespace org;
+
+
+class Helper
+{
+    /**
+     * @param $code
+     * @param string $data
+     * @param int $count
+     * @param bool $sign
+     * @param bool $isexit
+     * @return array
+     */
+    static function json_error($code, $data='',$count=0, $sign=false, $isexit=true){
+        if(empty($data)){
+            $data = array('msg'=>config('ERR_CODE.'.$code));
+        }
+
+        $return = array();
+        $return['code'] = $code;
+        $return['result'] = 'error';
+        $return['time'] = time();
+        if (is_array($data) && count($data) == 0){
+            $return['data'] = null;
+        } else {
+            $return['data'] = $data;
+        }
+
+        if($sign && is_array($data)){
+            $return['sign'] = create_s($data);
+        }
+
+        if($isexit){
+            header('Content-Type:application/json; charset='.config('DEFAULT_CHARSET'));
+            echo json_encode($return);
+            exit;
+        }else{
+            return $return;
+        }
+    }
+
+    /**
+     * @param array $data
+     * @param int $count
+     * @param bool $signasdasdasd
+     * @param bool $isexit
+     * @return array
+     */
+    static function json_success($data=[],$count=0, $sign=false, $isexit=true){
+        $return = array();
+        $return['code'] = 0;
+        $return['time'] = time();
+        if (is_array($data) && count($data) == 0) {
+            $return['data'] = null;
+        }else{
+            $return['data'] = $data;
+        }
+        if( $count !== false){
+            $return['count'] = $count;
+        }
+        if($sign && is_array($data)){
+            $return['sign'] = create_sign($data);
+        }
+
+        if($isexit){
+            header('Content-Type:application/json; charset='.config('DEFAULT_CHARSET'));
+            echo json_encode($return);
+            exit;
+        }else{
+            return $return;
+        }
+    }
+
+    /**
+     * 数据排序、删除杂项及空值
+     */
+    function para_filter_sort($para) {
+        $para_filter = array();
+        ksort($para);
+
+        foreach ($para as $key=>$val) {
+            //将key全部小写
+            $key = strtolower($key);
+            if ($key === "sign" || $key === "action" || $key === "_request" || $val === "" || is_null($val)) {
+                continue;
+            } else {
+                if (is_bool($val)) {
+                    $para_filter[$key] = (int)$val;
+                } else {
+                    $para_filter[$key] = is_array($val) ? para_filter_sort($val) : $val;
+                }
+            }
+        }
+
+        return $para_filter;
+    }
+
+    /**
+     * 生成拼接串
+     * @param string $para
+     * @return string
+     */
+    function create_link_string($para){
+        $arg = '';
+        foreach($para as $key => $val){
+            if(is_array($val)){
+                $arg .= $key . '=(' . (is_array($val) ? create_link_string($val) : $val) . ')&';
+            }else{
+                $arg .= $key . '=' . $val . '&';
+            }
+        }
+
+        if(count($arg) > 0){
+            //去掉最后一个&字符
+            $arg = substr($arg, 0, count($arg) - 2);
+        }
+
+        return $arg;
+    }
+
+    /**
+     * 生成校验字符串
+     * @param string $data
+     * @return string
+     */
+    function create_sign($data){
+        $arg = '';
+        if($data){
+            // 按照key对数组进行排
+            $data = para_filter_sort($data);
+            // 生成拼接串
+            $prestr = create_link_string($data);
+
+            //如果存在转义字符，那么去掉转义
+            if(get_magic_quotes_gpc()){
+                $arg = stripslashes($arg);
+            }
+
+            if(empty($prestr)){
+                return '';
+            }else{
+                $prestr .= C('API_KEY');
+                return md5($prestr);
+            }
+        }else{
+            return '';
+        }
+    }
+
+
+
+    /**
+     * url参数转化成数组
+     * @param string
+     * @return mixed
+     */
+    static function convertUrlArray($query)
+    {
+        $queryParts = explode('&', $query);
+        $params = array();
+        foreach ($queryParts as $param) {
+            $item = explode('=', $param);
+            $params[$item[0]] = $item[1];
+        }
+        return $params;
+    }
+
+    /**
+     * 中文url 将url中的中文转换成urlcode编码
+     */
+    static function chineseUrlToUrlCode($url){
+        $urlArr = explode('/',$url);
+        $newUrl = '';
+        foreach ($urlArr as $k => $v){
+            if(preg_match("/[\x7f-\xff]/",$v)){
+               $v = urlencode($v);
+            }
+
+            $newUrl .= $v.'/';
+        }
+
+        return rtrim($newUrl,'/');
+    }
+
+
+static function iunserializer($value) {
+        if (empty($value)) {
+            return array();
+        }
+        if (!is_serialized($value)) {
+            return $value;
+        }
+        $result = unserialize($value);
+        if ($result === false) {
+            $temp = preg_replace_callback('!s:(\d+):"(.*?)";!s', function ($matchs){
+                return 's:'.strlen($matchs[2]).':"'.$matchs[2].'";';
+            }, $value);
+            return unserialize($temp);
+        } else {
+            return $result;
+        }
+    }
+
+}
---
+++ extend/org/WxJsConfig.php
@@ -0,0 +1,130 @@
+<?php
+/**----------------------------------------------------------------------
+ * OpenEwei V1
+ * Copyright 2018-2018 http://www.redkylin.con All rights reserved.
+ * ----------------------------------------------------------------------
+ * Author: ewei(lamp_heyiwei@163.com)
+ * Date: 2019/1/9
+ * Time: 11:07
+ * ----------------------------------------------------------------------
+ */
+namespace org;
+
+class WxJsConfig
+{
+    static  $count=5;    //用于控制重复调用微信接口的次数，确保一定能调取成功
+    private $wxAppId;  //公众号的appid
+    private $wxAppSecret; //公众号的app_secret
+
+    public function __construct()
+    {
+        $this->wxAppId      = 'wx34c7a253ee8bb252';
+        $this->wxAppSecret  = '4d5e20de795b2e477754bad89482f43d';
+    }
+
+    /** 发送curl请求
+     * @param $url
+     * @param string $method
+     * @param array $requestData
+     * @return mixed
+     */
+    public function curlRequest($url,$method='get',$requestData=[])
+    {
+        try {
+            $curlHandle = curl_init();
+            curl_setopt($curlHandle, CURLOPT_URL, $url);
+            curl_setopt($curlHandle, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
+            if ($method == 'post') {
+                curl_setopt($curlHandle, CURLOPT_POST, true);
+                curl_setopt($curlHandle, CURLOPT_POSTFIELDS, $requestData);
+            }
+            $response = curl_exec($curlHandle);
+            curl_close($curlHandle);
+            return $response;
+        } catch (\Exception $e) {
+            exit('请求失败:' . $e->getMessage());
+        }
+    }
+
+    public function getJsConfig()
+    {
+        try{
+            //此URL是需要调用微信JS-SDK页面的url，由前端页面传过来的且包含请求参数部分
+            $url = $_GET['url'];
+            if(!$url){
+                exit('缺少必要参数');
+            }
+
+            $acc_url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='
+                .$this->wxAppId .'&secret='.$this->wxAppSecret;
+            $acc_url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx34c7a253ee8bb252&secret=4d5e20de795b2e477754bad89482f43d';
+
+            //TODO  实际开发中获取access_token要进行缓存，执行curlRequest前判断是否存在缓存，不存在才进行调用，建议缓存7200秒
+            $accToken = json_decode($this->curlRequest($acc_url),true) ;
+            if(!isset($accToken['access_token'])){
+                exit($accToken['errmsg']);
+            }
+            $accToken = $accToken['access_token'];
+            $ticket_url = 'https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token='.$accToken.'&type=jsapi';
+            $ticket = json_decode($this->curlRequest($ticket_url),true);
+            if(!isset($ticket['ticket'])){
+                return exit('微信服务器异常');
+            }
+            $ticket = $ticket['ticket'];
+            //生成随机字符串
+            $randStr   = '';
+            $str       = $ticket.$accToken;
+            $strLength = strlen($str);
+            for ($i=0;$i<15;$i++){
+                if($i%3==0){
+                    $randStr.=rand();
+                }
+                $randStr.=$str[rand(0,$strLength)];
+            }
+            $randStr.=rand();
+            $time = time();
+            $tempSort = [
+                'noncestr'=>$randStr,
+                'jsapi_ticket'=>$ticket,
+                'timestamp'=>$time,
+                'url'=>$url
+            ];
+            $keyStr = array_flip($tempSort);
+            //加密参数是按参数名排序，不是按值排序
+            ksort($tempSort,SORT_STRING);
+            $params = $tempSort;
+            $shaString = '';
+            foreach ($params as $key=>$val){
+                if($shaString==''){
+                    $shaString = $keyStr[$val].'='.$val;
+                }else{
+                    $shaString.='&'.$keyStr[$val].'='.$val;
+                }
+            }
+            $signature = sha1($shaString);
+            $jsConfig = [
+                'appId'=>$this->wxAppId,
+                'timestamp'=>$time,
+                'nonceStr'=>$randStr,
+                'signature'=>$signature,
+                //此处填写你需要调用的JS列表，比如这里是调用的微信获取地理位置
+                'jsApiList'=>['checkJsApi', 'openLocation','getLocation'],
+                'test'=>[
+                    'ticket'=>$ticket,
+                    'url'=>$url
+                ]
+            ];
+            return json_encode($jsConfig,true);
+        }catch (\Exception $e){
+            if(self::$count>0){     //重复调用，尽量请求成功
+                self::$count-=1;
+                $this->getJsConfig();
+            }else{
+                exit('微信服务器异常');
+            }
+        }
+
+    }
+
+}
